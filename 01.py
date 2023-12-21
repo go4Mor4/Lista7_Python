59e22f1cb49632e13732b76d971e71fd3579e60a
DavidSilveira80 <davidpscontato@gmail.com>
2023-12-21 20:54:38 -0300

    Primeira abordagem.

--- 01.py
+++ 01.py
@@ -5,21 +5,24 @@ def validaIP(ipAddress):
         if i < 0 or i > 255:
             return False
     return True
-arquivoEntrada = open('01_entrada.txt', 'r')
-linhas = arquivoEntrada.readlines()
-arquivoEntrada.close()
-ipsValidos = []
-ipsInvalidos = []
-for ip in linhas:
-    if validaIP(ip) == True:
-        ipsValidos.append(ip)
-    else:
-        ipsInvalidos.append(ip)
-arquivoSaida = open('01_saida.txt', 'w')
-arquivoSaida.write('[IPs Validos]\n')
-for i in range(len(ipsValidos)):
-    arquivoSaida.write(ipsValidos[i])
-arquivoSaida.write('\n[IPs Invalidos]\n')
-for i in range(len(ipsInvalidos)):
-    arquivoSaida.write(ipsInvalidos[i])
-arquivoSaida.close()
+
+if __name__ == '__main__':
+
+    arquivoEntrada = open('01_entrada.txt', 'r')
+    linhas = arquivoEntrada.readlines()
+    arquivoEntrada.close()
+    ipsValidos = []
+    ipsInvalidos = []
+    for ip in linhas:
+        if validaIP(ip) == True:
+            ipsValidos.append(ip)
+        else:
+            ipsInvalidos.append(ip)
+    arquivoSaida = open('01_saida.txt', 'w')
+    arquivoSaida.write('[IPs Validos]\n')
+    for i in range(len(ipsValidos)):
+        arquivoSaida.write(ipsValidos[i])
+    arquivoSaida.write('\n[IPs Invalidos]\n')
+    for i in range(len(ipsInvalidos)):
+        arquivoSaida.write(ipsInvalidos[i])
+    arquivoSaida.close()
